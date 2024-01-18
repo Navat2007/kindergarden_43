035f93c5deb685074f7451c5e2b71f1723150fcc
Navat2007 <navat2007@yandex.ru>
2024-01-18 22:30:10 +0300

    add

--- src/layout/public.layout.component.jsx
+++ src/layout/public.layout.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import { motion } from "framer-motion";
+import {Outlet, useLocation} from "react-router-dom";
+import {motion} from "framer-motion";
 
-import { PreloaderContext } from "../context";
+import {PreloaderContext} from "../context";
 import Preloader from "../components/public/preloader/preloader.component";
 
 import "../styles/public.layout.scss";
@@ -26,23 +26,25 @@ const PublicLayout = () => {
     }, [location]);
 
     return (
-        <PreloaderContext.Provider value={{ loading, setLoading }}>
-            <Preloader loading={loading} />
-            <Header />
-            <motion.main
-                className='page'
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ delay: 0.2, duration: 1 }}
-            >
-                <Outlet />
-                <Feedback />
-                <Resourses />
-                <MapSection />
-            </motion.main>
-            <Footer />
-        </PreloaderContext.Provider>
+        <div className={"page"}>
+            <PreloaderContext.Provider value={{loading, setLoading}}>
+                <Preloader loading={loading}/>
+                <Header/>
+                <motion.main
+                    className='page__content'
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    transition={{delay: 0.2, duration: 1}}
+                >
+                    <Outlet/>
+                    <Feedback />
+                    <Resourses />
+                    <MapSection />
+                </motion.main>
+                <Footer/>
+            </PreloaderContext.Provider>
+        </div>
     );
 };
 
