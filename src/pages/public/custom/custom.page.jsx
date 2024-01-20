76e25b31f83ddd5a39e57e135806c812300b3ac2
Savallera <phenix-vn@yandex.ru>
2024-01-20 13:52:57 +0300

    файлы

--- src/pages/public/custom/custom.page.jsx
+++ src/pages/public/custom/custom.page.jsx
@@ -6,7 +6,7 @@ import { isObject } from "lodash";
 import { computed } from "@preact/signals-react";
 import createDOMPurify from "dompurify";
 
-import useCustomPagesStore, {loadAllFiles} from "../../../store/public/customPagesStore";
+import useCustomPagesStore, { loadAllFiles } from "../../../store/public/customPagesStore";
 
 import Construction from "../../../components/public/cunstruction/construction";
 import BasicPage from "../../../components/public/basic.page/basic.page.component";
@@ -46,72 +46,72 @@ const CustomPage = ({ id }) => {
             isObject(store.item?.page) &&
             (store.item?.page?.content || store.item?.page?.files?.length > 0) ? (
                 <motion.section
-                    className='article section__wrap page__section-indent'
+                    className='section'
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ delay: 0.2, duration: 1 }}
                 >
-                    <h1 className='section__title'>{store?.item?.page?.title}</h1>
-                    <div
-                        className='section__main-text'
-                        dangerouslySetInnerHTML={{
-                            __html: DOMPurify.sanitize(store?.item?.page?.content),
-                        }}
-                    />
-                    {photo.value.length > 0 && (
-                        <div className='article__section'>
-                            <h2 className='section__subtitle'>Галерея</h2>
-                            <ImageGallery extraClass={"article__gallery"} items={photo.value} />
-                        </div>
-                    )}
-                    {video.value.length > 0 && (
-                        <div className='article__section'>
-                            <h2 className='article__subtitle'>Видео</h2>
-                            <VideoGallery items={video.value} />
-                        </div>
-                    )}
-                    {files.value.length > 0 && (
-                        <div className='article__section'>
-                            <h2 className='article__subtitle'>Документы</h2>
-                            <ul className='article__files'>
-                                {files.value.map((file) => (
-                                    <li key={window.global.makeid(6)}>
-                                        <a
-                                            href={
-                                                file.url.includes("http")
-                                                    ? file.url
-                                                    : process.env.REACT_APP_BASE_URL + file.url
-                                            }
-                                            target={"_blank"}
-                                            rel='noreferrer noopener nofollow'
-                                            className='article__file-link'
-                                        >
-                                            {FileIcons.default}
-                                            {file.title ? file.title : file.url}
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                            {
-                                files.value.length > 1
-                                &&
-                                <button
-                                    type='button'
-                                    className='article__button'
-                                    onClick={async () => {
-                                        await loadAllFiles({
-                                            id,
-                                            title: store.item.menu.title,
-                                            files: files.value.map(item => item.url)
-                                        });
-                                    }}
-                                >
-                                    Скачать все файлы
-                                </button>
-                            }
-                        </div>
-                    )}
+                    <div className='section__wrap page__section-indent'>
+                        <h1 className='section__title section__title_with-decor'>{store?.item?.page?.title}</h1>
+                        <div
+                            className='section__main-text'
+                            dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(store?.item?.page?.content),
+                            }}
+                        />
+                        {photo.value.length > 0 && (
+                            <div className='section__indent'>
+                                <h2 className='section__subtitle'>Галерея</h2>
+                                <ImageGallery extraClass={"section__card-deck"} items={photo.value} />
+                            </div>
+                        )}
+                        {video.value.length > 0 && (
+                            <div className='section__indent'>
+                                <h2 className='section__subtitle'>Видео</h2>
+                                <VideoGallery items={video.value} />
+                            </div>
+                        )}
+                        {files.value.length > 0 && (
+                            <div className='section__indent'>
+                                <h2 className='section__subtitle'>Документы</h2>
+                                <ul className='section__list'>
+                                    {files.value.map((file) => (
+                                        <li key={window.global.makeid(6)}>
+                                            <a
+                                                href={
+                                                    file.url.includes("http")
+                                                        ? file.url
+                                                        : process.env.REACT_APP_BASE_URL + file.url
+                                                }
+                                                target={"_blank"}
+                                                rel='noreferrer noopener nofollow'
+                                                className='file-item'
+                                            >
+                                                <span className="file-item__icon">{FileIcons.default}</span>
+                                                <span className="file-item__text">{file.title ? file.title : file.url}</span>
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                                {files.value.length > 1 && (
+                                    <button
+                                        type='button'
+                                        className='button button_theme_outline-main'
+                                        onClick={async () => {
+                                            await loadAllFiles({
+                                                id,
+                                                title: store.item.menu.title,
+                                                files: files.value.map((item) => item.url),
+                                            });
+                                        }}
+                                    >
+                                        <span className='button__text'>Скачать все файлы</span>
+                                    </button>
+                                )}
+                            </div>
+                        )}
+                    </div>
                 </motion.section>
             ) : (
                 <Construction />
